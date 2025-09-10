52b3aad608eed3046918dcd1e7eb60888fc53698
vanisha <169176947+VanishaParwal@users.noreply.github.com>
2025-09-11 04:13:57 +0530

    Add H2-based test profile for CI/CD compatibility

--- src/test/java/com/example/bankapp/BankappApplicationTests.java
+++ src/test/java/com/example/bankapp/BankappApplicationTests.java
@@ -2,7 +2,7 @@ package com.example.bankapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+@ActiveProfiles("test")
 @SpringBootTest
 class BankappApplicationTests {
 
